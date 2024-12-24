7cb2fff28022c6aaabe739298bd9e2bf423430f2
Jayrodri088 <johnbetho.c.088@gmail.com>
2024-12-24 10:35:36 +0100

    context

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React video for context API</h1>
+      <Login />
+      <Profile />
+    </>
+  )
+}
+
+export default App
---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>Profile: {}
+        <h1>More Component</h1>
+    </div>
+  )
+}
+
+export default Profile
