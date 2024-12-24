906b5872962653915d887fb23c0a0d0835bbdade
Jayrodri088 <johnbetho.c.088@gmail.com>
2024-12-24 09:38:15 +0100

    router update

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import {Outlet} from 'react-router-dom'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
