7291c9122faa53042e7cb59e3217d00db711d19e
Jayrodri088 <johnbetho.c.088@gmail.com>
2024-12-25 19:35:42 +0100

    implement todo management features with context API and local storage

---
+++ 10todo/src/contexts/TodoContext.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import { createContext, useContext} from 'react';
+
+export const TodoContext = createContext({
+    todos: [
+        { id: 1, todo: 'Todo 1', completed: false },
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleTodo: (id) => {},
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
+
+export const TodoProvider = TodoContext.Provider;
