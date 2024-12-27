06a5081b5ff18b0d423c30565f578211a2689444
Jayrodri088 <johnbetho.c.088@gmail.com>
2024-12-27 13:07:35 +0100

    add reusable components for layout and form elements

---
+++ 11blogApp/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Logo({width = "100%"}) {
+  return (
+    <img src="" alt="Logo Placeholder" style={width} />
+  )
+}
+
+export default Logo
