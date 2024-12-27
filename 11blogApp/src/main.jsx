d017a02ca0a912b639c90612b549b89a5c960fc3
Jayrodri088 <johnbetho.c.088@gmail.com>
2024-12-27 11:25:37 +0100

    add authentication and routing setup with Redux for blog application

--- 11blogApp/src/main.jsx
+++ 11blogApp/src/main.jsx
@@ -2,9 +2,83 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Provider} from 'react-redux'
+import store from './store/store.js'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import AuthLayout from './layouts/AuthLayout.jsx'
+import Signup from './pages/Signup.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import AddPost from './pages/AddPost.jsx'
+import EditPost from './pages/EditPost.jsx'
+import Post from './pages/Post.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/all-posts',
+        element: (
+          <AuthLayout authentication={true}>
+            <AllPosts />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/add-post',
+        element: (
+          <AuthLayout authentication={true}>
+            <AddPost />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/edit-post/:slug',
+        element: (
+          <AuthLayout authentication={true}>
+            <EditPost />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/post',
+        element: (
+          <AuthLayout authentication={true}>
+            <Post />
+          </AuthLayout>
+        )
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
---
+++ 11blogApp/src/pages/AllPosts.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function AllPosts() {
+  return (
+    <div>AllPosts</div>
+  )
+}
+
+export default AllPosts
